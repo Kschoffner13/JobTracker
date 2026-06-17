a410607d0ebeb654d09b626da0ad1c1dae4b8331
Kschoffner13 <kschoffner@gmail.com>
2026-06-17 13:42:13 -0600

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://job-tracker-six-lyart.vercel.app/",
+        "https://job-tracker-six-lyart.vercel.app",
+        "https://job-tracker-pzxqxt516-kschoffner-9548s-projects.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
